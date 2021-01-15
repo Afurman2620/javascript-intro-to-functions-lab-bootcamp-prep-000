92f0f5b89c0fa61451df22ad951dd48088cda6aa
Artem  F <artemfm@gmail.com>
2021-01-15 01:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma (str){
    return "I can't hear you!" ;
      if (str==str.toUpperCase())
    return "YES INDEED!"  ;
-   if(str==str"I love you, Grandma.")
-   return ""
+   if (str==str"I love you, Grandma.")
+   return "I love you, too."
 }
