d106cede9bb05cf8365767ce2261c8a3ef595416
Artem  F <artemfm@gmail.com>
2021-01-15 01:33:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(str) {
+}
+
+function shout(str) {
+  return str.toUpperCase()
+}
+
+function whisper(str) {
+  return str.toLowerCase()
+}
+
+function logShout(str){
+  console.log(str.toUpperCase())
+}
+
+function logWhisper(str){
+  console.log(str.toLowerCase())
+}
+
+function sayHiToGrandma (str){
+  if (str==str.toLowerCase())
+   return "I can't hear you!" 
+}
+
+function sayHiToGrandma (str){
+  if (str==str.toUpperCase())
+   return "YES INDEED!"   
+}
